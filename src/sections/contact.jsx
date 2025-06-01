c670084ca8a3712efa0882fb4fa3f623f77846b0
Andrew Li <amcli@uwaterloo.ca>
2025-05-31 22:23:57 -0400

    Updated contact

--- src/sections/contact.jsx
+++ src/sections/contact.jsx
@@ -6,6 +6,7 @@ export default function Contact() {
       <h2>Contact Me</h2>
       <p>Email: <a href="mailto:amcli@uwaterloo.ca">amcli@uwaterloo.ca</a></p>
       <p>LinkedIn: <a href="https://www.linkedin.com/in/andrewmcli/" target="_blank">linkedin.com/in/andrewmcli</a></p>
+      <p>Phone: <a>778-706-5362</a></p>
     </section>
   )
 }
