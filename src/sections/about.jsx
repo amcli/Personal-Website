fa47830e76485ea40bcd446be51b7693cc234e39
Andrew Li <amcli@uwaterloo.ca>
2025-05-31 22:21:23 -0400

    Updated about me

--- src/sections/about.jsx
+++ src/sections/about.jsx
@@ -4,7 +4,10 @@ export default function About() {
   return (
     <section id="about" className="about">
       <h2>About Me</h2>
-      <p>I’m a software developer passionate about building innovative user experiences, especially in the realm of VR and games. I'm currently studying Computer Engineering at the University of Waterloo.</p>
+      <p>I’m a software developer passionate about building innovative user experiences, especially in the realm of VR and games.
+        I also have a strong interest for AI/ML and Fullstack Development.
+      </p>
+      <p>I'm currently in my 1B term at the University of Waterloo for Computer Engineering.</p>
     </section>
   )
 }
