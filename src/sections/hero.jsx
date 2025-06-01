09f548047a5528d925bdac33eb204a234b67bbf6
amcli <amcli@uwaterloo.ca>
2025-05-31 22:28:34 -0400

    Updated hero

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -4,9 +4,9 @@ export default function Hero() {
   return (
     <section className="hero">
       <div className="hero-content">
-        <h1>Hello, I'm <span className="highlight">Andrew Li</span></h1>
-        <h2>Software, Game, and Fullstack Developer</h2>
-        <p>I build engaging digital experiences and products.</p>
+        <h1>Hello, I'm <span className="highlight">Andrew M C Li</span></h1>
+        <h2>Software and Fullstack Developer</h2>
+        <p>I love to build engaging and practical digital experiences!</p>
       </div>
     </section>
   )
